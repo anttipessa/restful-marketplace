aaef6e8c02dc233a59a7540e685969e99a96c053
Antti <antti.pessa@tuni.fi>
2020-03-13 15:03:48 +0000

    register with redux

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,121 +1,133 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { postRegister } from '../actions/register'
 import { Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 
-class RegisterForm extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        open: this.props.open,
-        username: '',
-        email: '',
-        pw: '',
-        pwconf: ''
-      }
-    }
-  
-    componentDidUpdate(prevProps) {
-      if (this.props.open !== prevProps.open) {
-        this.handleOpen()
-      }
-    }
-  
-    handleOpen = () => {
-      this.setState({ open: true })
-    }
-  
-    handleClose = () => {
-      this.setState({ open: false })
-    }
-  
-    handleChange = (e) => {
-      this.setState({ [e.target.name]: e.target.value })
-    }
-  
-    handleRegister = () => {
-      console.log('Registered')
-      // tähän joku fetch('/api/login' { METHOD: 'POST' }) ..
-      this.setState({ username: '', email: '', pw: '', pwconf: '', open: false })
-    }
-  
-    render() {
-      return (
-        <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">Register</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              To buy items on this website, please register now by giving the
-              required information here.
-            </DialogContentText>
-            <TextField
-              required
-              label="Username"
-              style={{ margin: 8 }}
-              fullWidth
-              margin="normal"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              name="username"
-              onChange={this.handleChange}
-              variant="outlined"
-              value={this.state.username}
-            />
-            <TextField
-              required
-              label="Email"
-              style={{ margin: 8 }}
-              fullWidth
-              margin="normal"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              name="email"
-              onChange={this.handleChange}
-              variant="outlined"
-              value={this.state.email}
-            />
-            <TextField
-              required
-              label="Password"
-              style={{ margin: 8 }}
-              fullWidth
-              margin="normal"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              type="password"
-              name="pw"
-              onChange={this.handleChange}
-              variant="outlined"
-              value={this.state.pw}
-            />
-            <TextField
-              required
-              label="Password confirmation"
-              style={{ margin: 8 }}
-              fullWidth
-              margin="normal"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              type="password"
-              name="pwconf"
-              onChange={this.handleChange}
-              variant="outlined"
-              value={this.state.pwconf}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleRegister} color="primary">
-              Register
-            </Button>
-          </DialogActions>
-        </Dialog>
-      )
+const mapDispatchToProps = (dispatch) => {
+  return {
+    postRegister: (url, payload) => dispatch(postRegister(url, payload))
+  }
+}
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: this.props.open,
+      username: '',
+      email: '',
+      password: '',
+      passwordconf: '',
     }
   }
 
-  export default RegisterForm;
+  componentDidUpdate(prevProps) {
+    if (this.props.open !== prevProps.open) {
+      this.handleOpen()
+    }
+  }
+
+  handleOpen = () => {
+    this.setState({ open: true })
+  }
+
+  handleClose = () => {
+    this.setState({ open: false })
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleRegister = () => {
+    const register = {
+      name: this.state.username,
+      email: this.state.email,
+      password: this.state.password
+    }
+    this.props.postRegister('/api/users', register)
+    this.setState({ username: '', email: '', password: '', passwordconf: '', open: false })
+  }
+
+  render() {
+    return (
+      <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Register</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To buy items on this website, please register now by giving the
+            required information here.
+            </DialogContentText>
+          <TextField
+            required
+            label="Username"
+            style={{ margin: 8 }}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            name="username"
+            onChange={this.handleChange}
+            variant="outlined"
+            value={this.state.username}
+          />
+          <TextField
+            required
+            label="Email"
+            style={{ margin: 8 }}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            name="email"
+            onChange={this.handleChange}
+            variant="outlined"
+            value={this.state.email}
+          />
+          <TextField
+            required
+            label="Password"
+            style={{ margin: 8 }}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            type="password"
+            name="password"
+            onChange={this.handleChange}
+            variant="outlined"
+            value={this.state.password}
+          />
+          <TextField
+            required
+            label="Password confirmation"
+            style={{ margin: 8 }}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            type="password"
+            name="passwordconf"
+            onChange={this.handleChange}
+            variant="outlined"
+            value={this.state.passwordconf}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose} color="primary">
+            Cancel
+            </Button>
+          <Button onClick={this.handleRegister} color="primary">
+            Register
+            </Button>
+        </DialogActions>
+      </Dialog>
+    )
+  }
+}
+const RegisterForm = connect(null, mapDispatchToProps)(Register)
+export default RegisterForm;
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import items from './itemlist'
 import loggedInUser from './login'
+import registerUser from './register'
 
 export default combineReducers({
   items,
-  loggedInUser
+  loggedInUser,
+  registerUser
 })

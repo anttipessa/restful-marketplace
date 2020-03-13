e868e8fb3c265ff68c9042bf354c98c611ef9ef1
Henri Hakkarainen <henri.hakkarainen@tuni.fi>
2020-03-13 15:05:30 +0200

    Added few new icons to navbar

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { AppBar, Tabs, Tab, } from '@material-ui/core';
 import PersonPinIcon from '@material-ui/icons/PersonPin';
 import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import CreateIcon from '@material-ui/icons/Create';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import { connect } from 'react-redux'
 import { postLogout } from '../actions/login'
 import RegisterForm from '../components/RegisterForm'
@@ -52,7 +55,7 @@ class ConnectNav extends React.Component {
               <Tab label="Second" />
               <Tab label="Third" />
               <Tab label="Account information" icon={<PersonPinIcon />} />
-              <Tab label="Logout" onClick={this.handleLogoutClick} />
+              <Tab label="Logout" icon={<ExitToAppIcon />} onClick={this.handleLogoutClick} />
             </Tabs>
           </AppBar>
         </div>
@@ -65,8 +68,8 @@ class ConnectNav extends React.Component {
               <Tab label="Main" icon={<ShoppingBasket />} />
               <Tab label="Second" />
               <Tab label="Third" />
-              <Tab label="Register" onClick={this.handleClick} />
-              <Tab label="Login" onClick={this.handleLoginClick} />
+              <Tab label="Register" icon={<CreateIcon />} onClick={this.handleClick} />
+              <Tab label="Login" icon={<VpnKeyIcon />} onClick={this.handleLoginClick} />
             </Tabs>
           </AppBar>
           <RegisterForm open={this.state.open} />

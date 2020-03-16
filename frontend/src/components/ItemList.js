c86ec7e3787d9f56032135a0532453d1e09ff89b
Antti <antti.pessa@tuni.fi>
2020-03-16 18:11:04 +0000

    update itemlist view

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { fetchItems, deleteItem, updateItem, addItem } from '../actions/items'
 import List from '@material-ui/core/List';
-import ListItem  from '@material-ui/core/ListItem';
+import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -11,10 +11,16 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+import CloseIcon from '@material-ui/icons/Close';
 
 const mapStateToProps = (state) => {
   return {
-    items: state.items
+    items: state.items,
   }
 }
 
@@ -32,24 +38,32 @@ class ItemList extends React.Component {
     super(props);
     this.props.fetchItems('/api/items/')
     this.state = {
-      opened: false,
+      open: false,
       name: "",
       price: "",
-      id: ""
+      id: "",
+      createOpen: false,
+      alert: false,
+      success: false
     }
   }
-  
+
   handleClick = (item) => {
     this.setState({
       name: item.name,
       price: item.price,
       id: item._id,
-      opened: true
+      open: true
     })
   }
+
   handleClose = () => {
     this.setState({
-      opened: false
+      open: false,
+      createOpen: false,
+      createName: '',
+      createPrice: '',
+      alert: false
     })
   }
   handleChange = (e) => {
@@ -57,29 +71,55 @@ class ItemList extends React.Component {
       [e.target.name]: e.target.value
     })
   }
+
   handleDelete = () => {
     fetch(`/api/items/${this.state.id}`, {
       method: 'DELETE'
     })
-    .then(res => res.json())
-    .then(data => {
-      this.props.deleteItem(data.deleted)
-      this.setState({ opened: false })
-    })
+      .then(res => res.json())
+      .then(data => {
+        this.props.deleteItem(data.deleted)
+        this.setState({ open: false, success: true, successMsg: 'Item deleted!' })
+      })
   }
-  handleUpdate = () => {
 
+  handleUpdate = () => {
+    const update = {}
+    if (this.state.name) update.name = this.state.name
+    if (this.state.price) update.price = this.state.price
+    fetch(`/api/items/${this.state.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(update)
+    })
+      .then(res => {
+        if (!res.ok) throw Error(res.statusText)
+        return res.json()
+      })
+      .then((data) => {
+        this.props.updateItem(data)
+        this.setState({
+          open: false,
+          alert: false,
+          success: true,
+          successMsg: 'Item updated!'
+        })
+      })
+      .catch(() => this.setState({ alert: true, alertMsg: 'Update failed - check information!' }))
   }
+
   handleAdd = () => {
 
   }
-  
+
 
   render() {
     if (this.props.items.isFetching === true) {
       return (
         <div>
-          <h1>Items currently on sale!</h1>
+          <h1>All items</h1>
           <p>Loading</p>
         </div>
       )
@@ -87,46 +127,67 @@ class ItemList extends React.Component {
     return (
       <div>
         <h1>All items</h1>
-        <Button variant="contained" onClick={this.handleAdd} color="primary">Add item</Button>
         <List>
           {this.props.items.items.map(item => (
             <ListItem button onClick={this.handleClick.bind(this, item)} divider={true} key={item._id} >
-            <ListItemText
-              primary={item.name}
-              secondary={
-                <span>
-                  <span>Price: {item.price} €</span>
-                  <br />
-                  <span>On sale: {item.onsale.toString()}</span>
-                </span>
-              }
-            />
-          </ListItem>
+              <ListItemText
+                primary={item.name}
+                secondary={
+                  <span>
+                    <span>Price: {item.price} €</span>
+                    <br />
+                    <span>On sale: {item.onsale.toString()}</span>
+                  </span>
+                }
+              />
+            </ListItem>
           ))}
         </List>
-        <Dialog open={this.state.opened} onClose={this.handleClose} aria-labelledby="form-dialog-title">
+        
+        <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
           <DialogTitle id="form-dialog-title">Update or delete item </DialogTitle>
           <DialogContent>
             <DialogContentText>You can change the name and price or delete the item.</DialogContentText>
-              <TextField
-                margin="dense"
-                label="name"
-                value={this.state.name}
-                name="name"
-                type="text"
-                onChange={this.handleChange}
-                fullWidth
-              />
-              <TextField
-                margin="dense"
-                label="price"
-                value={this.state.price}
-                name="price"
-                type="text"
-                onChange={this.handleChange}
-                fullWidth
-              />          
+            <TextField
+              margin="dense"
+              label="name"
+              value={this.state.name}
+              name="name"
+              type="text"
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              margin="dense"
+              label="price"
+              value={this.state.price}
+              name="price"
+              type="text"
+              onChange={this.handleChange}
+              fullWidth
+            />
           </DialogContent>
+
+          <Collapse in={this.state.alert}>
+            <Alert
+              severity="error"
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => {
+                    this.setState({ alert: false });
+                  }}
+                >
+                  <CloseIcon fontSize="inherit" />
+                </IconButton>
+              }
+            >
+              <AlertTitle>Error</AlertTitle>
+              {this.state.alertMsg}
+            </Alert>
+          </Collapse>
           <DialogActions>
             <Button onClick={this.handleClose} color="primary">
               Cancel
@@ -139,6 +200,13 @@ class ItemList extends React.Component {
             </Button>
           </DialogActions>
         </Dialog>
+
+
+        <Snackbar open={this.state.success} autoHideDuration={3000} onClose={this.successClose}>
+          <Alert onClose={this.successClose} severity="success" variant="filled">
+            {this.state.successMsg}
+          </Alert>
+        </Snackbar>
       </div>
     )
   }

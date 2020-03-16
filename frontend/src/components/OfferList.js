11b506d507dcedebae01285b8cf372e2456691c2
Antti <antti.pessa@tuni.fi>
2020-03-16 17:20:45 +0000

    fetch offers

--- frontend/src/components/OfferList.js
+++ frontend/src/components/OfferList.js
@@ -1,11 +1,105 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux'
+import { fetchItems } from '../actions/items'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-class OfferList extends React.Component {
-  render() {
-    return (
-      <h1>Currently offered</h1>
-    )
+const mapStateToProps = (state) => {
+  return {
+    items: state.items,
+    user: state.loggedInUser
   }
 }
 
-export default OfferList
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchItems: url => dispatch(fetchItems(url))
+  }
+}
+
+class ConnectedList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props.fetchItems('/api/items/offers')
+    this.state = {
+      name: '',
+      price: '',
+      owner: '',
+      open: false,
+    }
+  }
+
+  handleClick = (item) => {
+    this.setState({
+      name: item.name,
+      price: item.price,
+      owner: item.owner,
+      open: true
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    })
+  };
+
+
+  render() {
+    if (this.props.items.isFetching === true) {
+      return (
+        <div>
+          <h1>Items currently offered by users!</h1>
+          <p>Loading</p>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <h1>Items currently offered by users!</h1>
+        <List>
+          {this.props.items.items.map(item => (
+            <ListItem button divider={true} key={item._id} onClick={this.handleClick.bind(this, item)}>
+              <ListItemText
+                primary={item.name}
+                secondary={
+                  <span>
+                    <span>Price: {item.price} €</span>
+                    <br />
+                    <span>Seller: {item.owner.name}</span>
+                  </span>
+                }
+              />
+            </ListItem>
+          ))}
+        </List>
+        <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Item preview </DialogTitle>
+          <DialogContent>
+            <DialogContentText>You must register to buy this item.</DialogContentText>
+            <p>Name: {this.state.name}</p>
+            <p>Price: {this.state.price} €</p>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+          </Button>
+            <Button onClick={this.handleClose} disabled={!this.props.user.loggedIn}>
+              Buy
+          </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+const OfferList = connect(mapStateToProps, mapDispatchToProps)(ConnectedList)
+export default OfferList;
